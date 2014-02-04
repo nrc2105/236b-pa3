3600e9ff31cfc44963b4b25e1526eeed704ab928
Nicholas Cummins <nicholas.r.cummins@gmail.com>
2014-02-03 22:37:58 -0500

    Improved commenting

--- maze_designer.rb
+++ maze_designer.rb
@@ -2,7 +2,7 @@ require_relative 'maze'
 require_relative 'node'
 
 class MazeDesigner
-	#
+	#Takes all relevent maze attributes to restructure
 	def initialize(maze)
 		@maze = maze
 		@width = maze.width
@@ -10,7 +10,7 @@ class MazeDesigner
 		@nodes = maze.nodes
 		@maze_array = maze.maze_array
 	end
-	#
+	#Essentially resets maze, then uses a random DFS to construct new maze, then loads new maze
 	def design
 		@maze.construct_default_maze_string
 		@maze.construct_node_matrix
@@ -19,13 +19,13 @@ class MazeDesigner
 		create_maze_string
 		@maze.load(@maze_string)
 	end
-	#
+	#Creates the final maze string for loading
 	def create_maze_string
 		convert_array_for_load
 		@maze_string = ""
 		@maze_array.each{|row| row.each{|element| @maze_string += element}}
 	end
-	#
+	#Converst the array of the maze to 1's and 0's
 	def convert_array_for_load
 		@maze_array.each do |row| 
 			row.each_index do |i|
@@ -34,7 +34,7 @@ class MazeDesigner
 			end
 		end
 	end
-
+	#Performs the random DFS to determine the new maze, backtracking when no way forward exists
 	def random_depth_first_search(startX, startY, endX, endY)
 		node = @nodes[startY][startX]
 		while node != @nodes[endY][endX]
@@ -47,7 +47,7 @@ class MazeDesigner
 			end
 		end
 	end
-
+	#Determines if there is another node in the adjacency list and if so returns a random one
 	def find_next(node)
 		while node.adjacent.size > 0
 			next_node = node.adjacent[rand(node.adjacent.size)]
@@ -59,7 +59,7 @@ class MazeDesigner
 		end
 		return false
 	end
-
+	#Removes a wall between cells
 	def knock_down_wall(node1, node2)
 		if node1.x == node2.x
 			@maze_array[node1.y + node2.y + 1][node1.x * 2 + 1] = " "
@@ -67,8 +67,7 @@ class MazeDesigner
 			@maze_array[node1.y * 2 + 1][node1.x + node2.x + 1] = " "
 		end
 	end
-
-
+	#Connects all nodes to start to allow for DFS
 	def connect_all_nodes
 		@nodes.each do |row|
 			row.each do |node|
@@ -79,22 +78,22 @@ class MazeDesigner
 			end
 		end
 	end
-
+	#Connects node to node above
 	def connect_above(node)
 		above_node = @nodes[node.y - 1][node.x] unless node.y == 0
 		node.add_adjacent(above_node)
 	end
-
+	#Connects node to node below
 	def connect_below(node)
 		below_node = @nodes[node.y + 1][node.x] unless node.y + 1 == @nodes.size
 		node.add_adjacent(below_node)
 	end
-
+	#Connects node to node on left
 	def connect_left(node)
 		left_node = @nodes[node.y][node.x - 1] unless node.x == 0
 		node.add_adjacent(left_node)
 	end
-
+	#Connects node to node on right
 	def connect_right(node)
 		right_node = @nodes[node.y][node.x + 1] unless node.x + 1 == @nodes[0].size
 		node.add_adjacent(right_node)
