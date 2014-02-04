1b4d004624b99f49244a3807163320e9b4b5eafb
Nicholas Cummins <nicholas.r.cummins@gmail.com>
2014-02-03 21:43:45 -0500

    Refactored Designer

--- maze_designer.rb
+++ maze_designer.rb
@@ -14,8 +14,6 @@ class MazeDesigner
 		@maze.construct_default_maze_string
 		@maze.construct_node_matrix
 		connect_all_nodes
-		@maze.find_node_adjacency
-		puts "1,1 is adjacent to #{@nodes[1][1].adjacent.size}"
 
 		#maze_string = "1" * width
 		#for i in 0..((height * width) - 2 * width - 1)
@@ -29,28 +27,33 @@ class MazeDesigner
 	def connect_all_nodes
 		@nodes.each do |row|
 			row.each do |node|
-			connect_vertical(node)
-			connect_horizontal(node)
+			connect_above(node)
+			connect_below(node)
+			connect_left(node)
+			connect_right(node)
 			end
 		end
 	end
 
-	def connect_vertical(node)
+	def connect_above(node)
 		above_node = @nodes[node.y - 1][node.x] unless node.y == 0
 		node.add_adjacent(above_node)
+	end
+
+	def connect_below(node)
 		below_node = @nodes[node.y + 1][node.x] unless node.y + 1 == @nodes.size
 		node.add_adjacent(below_node)
-	
 	end
 
-	def connect_horizontal(node)
+	def connect_left(node)
 		left_node = @nodes[node.y][node.x - 1] unless node.x == 0
 		node.add_adjacent(left_node)
-		right_node = @nodes[node.y][node.x + 1] unless node.x + 1 == @nodes[0].size
-		node.add_adjacent(right_node)
-	
 	end
 
+	def connect_right(node)
+		right_node = @nodes[node.y][node.x + 1] unless node.x + 1 == @nodes[0].size
+		node.add_adjacent(right_node)
+	end
 
 
 end
